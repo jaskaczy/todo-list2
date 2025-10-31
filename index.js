c105cfa6fe1e7e8cafe0b0f72a3c41869a5dd26f
jaskaczy <fjaskaczek@gmail.com>
2025-10-31 06:21:26 +0100

    trying smth

--- index.js
+++ index.js
@@ -19,31 +19,39 @@ button.addEventListener('click', ()=>{
 
   tasks.forEach(updateIds)
   console.log(tasks);
-  
+  return obj
   
 
 })
 function updateIds(element, index, array){
   element.id = index
 }
+function createObj(){
+  
+}
 
 
 
-
-function createTask(){
+function createTask(obj){
   let taskCont=document.createElement('div')
   let pCon=document.createElement('div')
+  pCon.dataset.id=obj.id
   let newParaghraph=document.createElement('p')
+  newParaghraph.textContent=obj.text
+  pCon.appendChild(newParaghraph)
   let optionsCont=document.createElement('div')
   let done=document.createElement('div')
+  optionsCont.appendChild(done)
   done.textContent='✅'
   let del=document.createElement('div')
   del.textContent='❎'
+  pCon.appendChild(del)
   let edit=document.createElement('div')
   edit.textContent='✍'
-  
-
-  return taskCont
+  pCon.appendChild(edit)
+  taskCont.appendChild(pCon)
+  taskCont.appendChild(optionsCont)
+  console.log(taskCont);
   
 }
 
